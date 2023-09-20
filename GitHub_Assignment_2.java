2f3deb32f752458fb899a7bf802bd18682fe74a0
Pınar Büyükcebeci <114187721+pinarbuyukcebeci@users.noreply.github.com>
2023-09-20 13:55:01 +0300

    Update GitHub_Assignment_2.java
    menu options

--- GitHub_Assignment_2.java
+++ GitHub_Assignment_2.java
@@ -3,4 +3,50 @@ import java.util.Random;
 
 
 public class GitHub_Assignment_2 {
+  
+    public static void main(String[] args) {
+        boolean again = true;
+        while(again)
+        {
+            // Take choice input
+            System.out.println("\nPlease choose what you want to find:");
+            System.out.println("-------------------------------");
+            System.out.println("1) Minimum of the array:");
+            System.out.println("2) Maximum of the array:");
+            System.out.println("3) Difference from the average:");
+            System.out.println("4) The sum of the odd indexes");
+            System.out.println("5) The sum of the even indexes");
+            System.out.println("6) Exit");
+            System.out.println("-------------------------------");
+            System.out.print("Enter choice: ");
+            int choice = in.nextInt();
+
+
+            if (choice == 1)
+            {
+                System.out.print("The minimum element of the list is: ");
+            }
+            else if (choice == 2)
+            {
+                System.out.print("The maximum element of the list is: ");
+            }
+            else if (choice == 3)
+            {
+                System.out.print("The differences of elements of the list from average are: ");  
+            }
+            else if (choice == 4)
+            {
+                System.out.print("The sums of elements on odd indexes: ");  
+            }
+            else if (choice == 5)
+            {
+                System.out.print("The sums of elements on even indexes: ");  
+            }
+            else if (choice == 6)
+            {
+                again = false;
+            }
+        }
+    }
+
 }
